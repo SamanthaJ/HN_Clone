fd3765ad5f710b4e33953a7323b0f5edf725a1b4
Samantha Jaromirski <samanthajaromirski@gmail.com>
2014-11-26 11:59:52 -0500

    migrates Post model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141126165210) do
+ActiveRecord::Schema.define(version: 20141126165921) do
+
+  create_table "posts", force: true do |t|
+    t.string   "link"
+    t.integer  "user_id"
+    t.integer  "votes"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
---
+++ spec/factories/posts.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :post do
+    link "MyString"
+user_id 1
+votes 1
+  end
+
+end
