fffbde45bf8d12aeaa3b19c746855f7f1c4ae49d
Samantha Jaromirski <samanthajaromirski@gmail.com>
2014-12-01 10:20:57 -0500

    adds title column to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,34 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
+  def index
+    @posts = Post.all
+    @post = Post.new
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @posts = Post.all
+    @post = current_user.posts.new(post_params)
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @post }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def show
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:link, :votes)
+  end
 end
