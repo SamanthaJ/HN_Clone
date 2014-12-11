8faf31fb583e2da0cbea294d064ce8b63e7cded1
Samantha Jaromirski <samanthajaromirski@gmail.com>
2014-12-11 11:24:17 -0500

    comment resource generated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141211013321) do
+ActiveRecord::Schema.define(version: 20141211162351) do
+
+  create_table "comments", force: true do |t|
+    t.integer  "post_id"
+    t.integer  "user_id"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "link"
