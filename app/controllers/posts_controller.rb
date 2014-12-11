de5504d7700560634319a819fa01f591a5d6d8ed
Samantha Jaromirski <samanthajaromirski@gmail.com>
2014-12-10 20:28:53 -0500

    Removing acts_as_commentable gem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,7 +33,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @user_who_commented = current_user
-    @comment = Comment.build_from( @post, @user_who_commented.id, "Hey guys this is my comment!" )
+    @comment = Comment.new
   end
 
   def upvote
